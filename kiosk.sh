870b18afd443e713142f76dbdd6d1127d05c2b09
Levi Durfee <levi.durfee@gmail.com>
2020-12-01 13:28:27 -0500

    Update kiosk script

--- kiosk.sh
+++ kiosk.sh
@@ -8,11 +8,6 @@ xset s noblank
 xset s off
 xset -dpms
 
-# Start unclutter
-unclutter -idle 0.5 -root &
-
 # Adjust Chromium settings
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
-
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk $KIOSK_URL &
