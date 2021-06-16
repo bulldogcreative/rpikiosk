2ccc84d86d343a4a2394947da3cf5c15b27de444
Levi Durfee <levi.durfee@gmail.com>
2021-06-16 11:21:03 -0400

    Set all locales

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export LC_ALL="en_US.UTF-8"
+echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/environment
+echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
+echo "LANG=en_US.UTF-8" | sudo tee -a /etc/locale.conf
+sudo locale-gen en_US.UTF-8
 
 # Run apt update before anything else.
 sudo apt update -y
