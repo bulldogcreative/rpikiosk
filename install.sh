84e9ea4e613557438eca7e3dd4277dac3d0e2f61
Levi Durfee <levi.durfee@gmail.com>
2021-06-16 12:21:06 -0400

    Try raspi-config util

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/environment
-echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
-echo "LANG=en_US.UTF-8" | sudo tee -a /etc/locale.conf
-sudo locale-gen en_US.UTF-8
+RC="sudo raspi-config nonint"
 
-reset
+${RC} do_configure_keyboard us
+${RC} do_change_timezone America/New_York
+${RC} do_change_locale LANG=en_US.UTF-8
 
 # Run apt update before anything else.
 sudo apt update -y
@@ -24,10 +23,11 @@ source ~/.env
 if [[ -z $PI_HOSTNAME ]]; then
     echo "Skipping hostname"
 else
+    ${RC} do_hostname ${PI_HOSTNAME}
     # https://raspberrypi.stackexchange.com/a/9900
-    sudo su -c 'source /home/pi/.env ; echo "127.0.0.1    $PI_HOSTNAME" >> /etc/hosts'
-    sudo su -c 'source /home/pi/.env ; echo $PI_HOSTNAME > /etc/hostname'
-    sudo su -c 'source /home/pi/.env ; hostname $PI_HOSTNAME'
+    #sudo su -c 'source /home/pi/.env ; echo "127.0.0.1    $PI_HOSTNAME" >> /etc/hosts'
+    #sudo su -c 'source /home/pi/.env ; echo $PI_HOSTNAME > /etc/hostname'
+    #sudo su -c 'source /home/pi/.env ; hostname $PI_HOSTNAME'
 fi
 
 # 888       888 d8b 8888888888 d8b
@@ -39,8 +39,9 @@ fi
 # 8888P   Y8888 888 888        888
 # 888P     Y888 888 888        888
 
-envsubst < ./configs/wpa_supplicant.conf >> ./wpa_supplicant.conf
-sudo su -c "cat ./wpa_supplicant.conf >> /etc/wpa_supplicant/wpa_supplicant.conf"
+#envsubst < ./configs/wpa_supplicant.conf >> ./wpa_supplicant.conf
+#sudo su -c "cat ./wpa_supplicant.conf >> /etc/wpa_supplicant/wpa_supplicant.conf"
+${RC} do_wifi_ssid_passphrase ${WIFI_SSID} ${WIFI_PSK}
 
 #  .d8888b.   .d8888b.  888    888
 # d88P  Y88b d88P  Y88b 888    888
