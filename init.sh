8b1938e9313059171b902bbdf8acffbe4c04c305
Levi Durfee <levi.durfee@gmail.com>
2021-08-10 13:03:00 -0400

    Add hostname to hosts file

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ source ~/.env
 if [[ -z $PI_HOSTNAME ]]; then
     echo "Skipping hostname"
 else
+    sudo su -c 'source /home/pi/.env ; echo "127.0.0.1    $PI_HOSTNAME" >> /etc/hosts'
     sudo hostnamectl set-hostname ${PI_HOSTNAME}
 fi
 
